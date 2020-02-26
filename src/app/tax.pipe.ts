7d060148e9e2566b2d70d6dfb505319c66653df5
Vignesh <vigneshm.cse2010@gmail.com>
2020-02-26 17:12:23 +0530

    Fixed bug in tax pipe

--- src/app/tax.pipe.ts
+++ src/app/tax.pipe.ts
@@ -8,7 +8,7 @@ export class TaxPipe implements PipeTransform {
   transform(value: any, ...args: any[]): any {
     let gst = 0.12
     let courierCharge = 150
-    return value * gst + courierCharge;
+    return value + (value * gst) + courierCharge;
   }
 
 }
